f4b241c9a667e8c4f3091764e11fc14f4db62e6f
Gonzalo <34680110ax@gmail.com>
2024-11-10 19:04:55 -0300

    dvcd

--- src/api/axios/axios.js
+++ src/api/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosconfig = axios.create({
-    baseURL: "http://localhost:8080",  // Verifica que esta URL sea la correcta
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json"
     }
